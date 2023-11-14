d531f7b481de88ae0c7d527c22a9c8767bf409bc
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-13 21:06:21 -0300

    Initial Commit

---
+++ apple-frameworks/apple_frameworksApp.swift
@@ -0,0 +1,17 @@
+//
+//  apple_frameworksApp.swift
+//  apple-frameworks
+//
+//  Created by Agustin Carbajal on 13/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct apple_frameworksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
