0e91e96c08c7a14fb4cb9454b40f950cafa1693d
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-14 20:52:12 -0300

    [feature] grid view

--- apple-frameworks/FrameworkGridView.swift
+++ apple-frameworks/FrameworkGridView.swift
@@ -8,13 +8,45 @@
 import SwiftUI
 
 struct FrameworkGridView: View {
+    
+    let columns: [GridItem] = [GridItem(.flexible()),
+                               GridItem(.flexible()),
+                               GridItem(.flexible())]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        LazyVGrid(columns: columns) {
+            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
+            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
+            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
+            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
+            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
+            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
+        }
     }
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
         FrameworkGridView()
+    }
+}
+
+struct FrameworkTitleView: View {
+    
+    let name: String
+    let imageName: String
+    
+    var body: some View {
+        VStack{
+            Image(imageName)
+                .resizable()
+                .frame(width: 90, height: 90)
+            Text(name)
+                .font(.title2)
+                .fontWeight(.semibold)
+                .scaledToFit()
+                .minimumScaleFactor(0.6) // puede hacer un shrink de hasta el 60% del tamaño original
+        }
     }
 }
