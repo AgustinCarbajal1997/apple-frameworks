2980486c2e2ce0ef9da541818e3f1b02c9665934
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-15 15:14:59 -0300

    [feature] modal view

---
+++ apple-frameworks/AFButton.swift
@@ -0,0 +1,20 @@
+//
+//  AFButton.swift
+//  apple-frameworks
+//
+//  Created by Agustin Carbajal on 15/11/2023.
+//
+
+import SwiftUI
+
+struct AFButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AFButton_Previews: PreviewProvider {
+    static var previews: some View {
+        AFButton()
+    }
+}
---
+++ apple-frameworks/FrameworkDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkDetailView.swift
+//  apple-frameworks
+//
+//  Created by Agustin Carbajal on 15/11/2023.
+//
+
+import SwiftUI
+
+struct FrameworkDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkDetailView()
+    }
+}
