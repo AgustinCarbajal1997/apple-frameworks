6e6a8f81eb0b34517eaf842cc56a16aab1da568e
AgustinCarbajal1997 <magustincarbajal97@gmail.com>
2023-11-14 21:40:07 -0300

    [featture] grid view

--- apple-frameworks/FrameworkGridView.swift
+++ apple-frameworks/FrameworkGridView.swift
@@ -14,39 +14,40 @@ struct FrameworkGridView: View {
                                GridItem(.flexible())]
     
     var body: some View {
-        
-        LazyVGrid(columns: columns) {
-            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
-            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
-            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
-            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
-            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
-            FrameworkTitleView(name: "app-clip", imageName: "app-clip")
+        NavigationView {
+            ScrollView {
+                LazyVGrid(columns: columns) {
+                    ForEach(MockData.frameworks, id: \.id) {
+                        framework in
+                        FrameworkTitleView(framework: framework)
+                    }
+                }
+            }
+            .navigationTitle("Frameworks")
         }
     }
 }
 
 struct FrameworkGridView_Previews: PreviewProvider {
     static var previews: some View {
-        FrameworkGridView()
+        FrameworkGridView().preferredColorScheme(.dark)
     }
 }
 
 struct FrameworkTitleView: View {
     
-    let name: String
-    let imageName: String
+    let framework: Framework
     
     var body: some View {
         VStack{
-            Image(imageName)
+            Image(framework.imageName)
                 .resizable()
                 .frame(width: 90, height: 90)
-            Text(name)
+            Text(framework.name)
                 .font(.title2)
                 .fontWeight(.semibold)
                 .scaledToFit()
                 .minimumScaleFactor(0.6) // puede hacer un shrink de hasta el 60% del tamaño original
-        }
+        }.padding()
     }
 }
